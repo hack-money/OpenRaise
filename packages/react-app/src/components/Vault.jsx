a70eb0df573756cdfedb54d1f75843c4edc2db3c
thomas-waite <thomas.waite4@gmail.com>
2021-10-23 16:50:01 +0100

    feat: populate vault creation form fields

--- packages/react-app/src/components/Vault.jsx
+++ packages/react-app/src/components/Vault.jsx
@@ -1,17 +1,8 @@
 import { utils, ethers } from "ethers";
 import { Button, Input, Form, Select, InputNumber, Table, Radio } from "antd";
 import React, { useState, useEffect } from "react";
-import { useContractLoader, useOnBlock } from "eth-hooks";
-import { NETWORKS } from "../constants";
-import { Transactor } from "../helpers";
 
 export default function Vault({ address, userSigner }) {
-  const selectedChainId =
-    userSigner && userSigner.provider && userSigner.provider._network && userSigner.provider._network.chainId;
-
-  const tx = Transactor(userSigner);
-
-
   const { Option } = Select;
   const formItemLayout = {
     labelCol: {
@@ -41,6 +32,7 @@ export default function Vault({ address, userSigner }) {
         <h3>Provide the settings for vault creation and click 'Submit'!</h3>
         <br />
         <br />
+        <h3>Vault settings</h3>
         <Form
           {...formItemLayout}
           form={form}
@@ -49,36 +41,211 @@ export default function Vault({ address, userSigner }) {
           initialValues={{ assetType: "eth" }}
         >
           <Form.Item
-            name="assetType"
-            label="Select Asset Type"
+            name="pairName"
+            label="Pair name"
             rules={[
               {
                 required: true,
               },
             ]}
           >
-            <Select placeholder="Select an asset type" onChange={onAssetChange} allowClear>
-              <Option value="eth">ETH</Option>
-              <Option disabled value="erc20">
-                ERC-20
-              </Option>
-            </Select>
+            <Input placeholder="UMA Range Dai"></Input>
           </Form.Item>
-          <Form.Item name="address" label="Address">
-            <Input disabled placeholder={address} />
-          </Form.Item>
+
           <Form.Item
-            name="amount"
-            label="Amount to bridge"
+            name="expirationTimeStamp"
+            label="Expiration time stamp"
             rules={[
               {
                 required: true,
               },
             ]}
           >
-            <InputNumber />
+            <Input placeholder="23/12/2023"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="collateralPerPair"
+            label="Collateral per pair"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={20000}></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            name="priceIdentifier"
+            label="Price identifier"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="Maker Oracle"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="longSynthName"
+            label="Long Synth Name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="UMARangeDai102"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="longSynthSymbol"
+            label="Long synth symbol"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="UD2"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="shortSynthName"
+            label="Short synth name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="UMARangeDai101"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="shortSynthSymbol"
+            label="Short synth symbol"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="UD1"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="collateralToken"
+            label="Collateral token"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input placeholder="Dai"></Input>
+          </Form.Item>
+
+          <Form.Item
+            name="prepaidProposerReward"
+            label="Prepaid proposer reward"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={800}></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            name="optimisticOracleLivenessTime"
+            label="Optimistic oracle liveness time"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={120}></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            name="optimisticOracleProposerBond"
+            label="Optimistic oracle proposer bond"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={300}></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            name="strikePrice"
+            label="Strike price"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={23}></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            name="basePercentage"
+            label="Base percentage"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={10}></InputNumber>
           </Form.Item>
         </Form>
+        
+        <br />
+        <br />
+        <br />
+        <h3>Tokens to mint</h3>
+        
+        <Form
+          {...formItemLayout}
+          form={form}
+        >
+          <Form.Item 
+            name="tokensToMint"
+            label="Num tokens to mint"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <InputNumber defaultValue={500}></InputNumber>
+          </Form.Item>
+        </Form>
+
+        <div>
+          <Button
+              style={{ 
+                marginTop: 50,
+                width: 150,
+                height: 100,
+                backgroundColor: '#FFBE0B'
+              }}
+              onClick={() => {
+                // TODO: Use Safe SDK to send a transaction
+              }}
+            >
+              Create
+          </Button>
+        </div>
       </div>
     </div>
   );
