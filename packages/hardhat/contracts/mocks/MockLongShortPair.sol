1dce4a63a91ac5ed4c44cae28dffd220bea4c86f
Tom French <tom@tomfren.ch>
2021-10-23 21:11:02 +0100

    feat: add mocked UMA

---
+++ packages/hardhat/contracts/mocks/MockLongShortPair.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/preset/ERC20PresetMinterPauser.sol";
+import "../interfaces/ILongShortPair.sol";
+
+contract MockLongShortPair is ILongShortPair {
+    IERC20 public collateral;
+    IERC20 public longToken;
+    IERC20 public shortToken;
+
+    uint256 collateralPerPair;
+
+    constructor(CreatorParams memory params) {
+        collateral = params.collateral;
+        longToken = new ERC20PresetMinterPauser(params.longSynthName, params.longSynthSymbol);
+        shortToken = new ERC20PresetMinterPauser(params.shortSynthName, params.shortSynthSymbol);
+        collateralPerPair = params.collateralPerPair
+    }
+
+    /**
+     * @notice Creates a pair of long and short tokens equal in number to tokensToCreate. Pulls the required collateral
+     * amount into this contract, defined by the collateralPerPair value.
+     * @dev The caller must approve this contract to transfer `tokensToCreate * collateralPerPair` amount of collateral.
+     * @param tokensToCreate number of long and short synthetic tokens to create.
+     * @return collateralUsed total collateral used to mint the synthetics.
+     */
+    function create(uint256 tokensToCreate) external returns (uint256 collateralUsed) {
+        collateral.transferFrom(msg.sender, address(this), tokensToCreate * collateralPerPair)
+        longToken.mint(msg.sender, tokensToCreate);
+        shortToken.mint(msg.sender, tokensToCreate);
+
+        return tokensToCreate * collateralPerPair;
+    }
+
+    /**
+     * @notice Redeems a pair of long and short tokens equal in number to tokensToRedeem. Returns the commensurate
+     * amount of collateral to the caller for the pair of tokens, defined by the collateralPerPair value.
+     * @dev This contract must have the `Burner` role for the `longToken` and `shortToken` in order to call `burnFrom`.
+     * @dev The caller does not need to approve this contract to transfer any amount of `tokensToRedeem` since long
+     * and short tokens are burned, rather than transferred, from the caller.
+     * @param tokensToRedeem number of long and short synthetic tokens to redeem.
+     * @return collateralReturned total collateral returned in exchange for the pair of synthetics.
+     */
+    function redeem(uint256 tokensToRedeem) external returns (uint256 collateralReturned) {
+        return 0;
+    };
+}
